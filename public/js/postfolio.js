ecd694b98e2bfeecffad6e0c260fc349112f74a3
Tomas Turi <tomasturi@gmail.com>
2022-10-31 15:13:13 -0300

    first commit

---
+++ public/js/postfolio.js
@@ -0,0 +1,14 @@
+var options = {
+  strings: ['Desarrollador <b>Full Stack</b>',
+            'HTML + CSS + Javascript', 
+            'Angular + JAVA',
+            'Argentina Programa 4.0',
+            ],
+  typeSpeed: 80,
+  backSpeed: 40,
+  backDelay: 200,
+  startDelay: 200,
+  loop: true,
+};
+
+var typed = new Typed('.element', options);
